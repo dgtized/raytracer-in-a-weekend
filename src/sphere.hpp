e6bd0c7f12e40d9844c38a815dfd97cbce23bf25
Charles Comstock <dgtized@gmail.com>
2020-08-19 19:29:01 -0500

    Calculate which side of a face we hit for normal intersection?

--- src/sphere.hpp
+++ src/sphere.hpp
@@ -31,7 +31,8 @@ bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) cons
         if (temp < t_max && temp > t_min) {
             rec.t = temp;
             rec.p = r.at(rec.t);
-            rec.normal = (rec.p - center) / radius;
+            vec3 outward_normal = (rec.p - center) / radius;
+            rec.set_face_normal(r, outward_normal);
             return true;
         }
 
@@ -39,7 +40,8 @@ bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) cons
         if (temp < t_max && temp > t_min) {
             rec.t = temp;
             rec.p = r.at(rec.t);
-            rec.normal = (rec.p - center) / radius;
+            vec3 outward_normal = (rec.p - center) / radius;
+            rec.set_face_normal(r, outward_normal);
             return true;
         }
     }
