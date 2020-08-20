7c4a0c9672057ed5a6b1236084869fc9d119941d
Charles Comstock <dgtized@gmail.com>
2020-08-19 19:44:42 -0500

    Introduce hittable_list to mimic a world of objects

--- src/main.cc
+++ src/main.cc
@@ -1,17 +1,15 @@
-#include "vec3.hpp"
-#include "ray.hpp"
+#include "rtweekend.hpp"
+
 #include "color.hpp"
-#include "hittable.hpp"
+#include "hittable_list.hpp"
 #include "sphere.hpp"
 
 #include <iostream>
 
-
-color ray_color(const ray& r, const hittable& surface ) {
+color ray_color(const ray& r, const hittable& world ) {
   hit_record rec;
-  if (surface.hit(r, 0, 1, rec)) {
-    vec3 N = rec.normal;
-    return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
+  if (world.hit(r, 0, infinity, rec)) {
+    return 0.5 * (rec.normal + color(1,1,1));
   }
   vec3 unit_direction = unit_vector(r.direction());
   auto t = 0.5*(unit_direction.y()+ 1.0);
@@ -25,6 +23,11 @@ int main() {
   const auto aspect_ratio = 16.0 / 9.0;
   const int image_width = 400;
   const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+  // World
+  hittable_list world;
+  world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
+  world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
 
   // Camera
   auto viewport_height = 2.0;
@@ -40,15 +43,13 @@ int main() {
 
   std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
-  auto surface = sphere(point3(0,0,-1), 0.5);
-
   for (int j = image_height-1; j >= 0; --j) {
     std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
     for (int i = 0; i < image_width; ++i) {
       auto u = double(i) / (image_width-1);
       auto v = double(j) / (image_height-1);
       ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
-      color pixel_color = ray_color(r, surface);
+      color pixel_color = ray_color(r, world);
       write_color(std::cout, pixel_color);
     }
   }
