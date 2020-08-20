a4b5dfbd9bd629db1f248c40d0dd29febf131e6f
Tessa Kelly <tessakkelly@gmail.com>
2020-08-19 18:13:02 -0700

    Adds random helpers

--- src/rtweekend.hpp
+++ src/rtweekend.hpp
@@ -3,6 +3,7 @@
 
 #include <cmath>
 #include <cstdlib>
+#include <random>
 #include <limits>
 #include <memory>
 
@@ -22,6 +23,28 @@ const double pi = 3.1415926535897932385;
 
 inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
+}
+
+inline double random_double() {
+    // Returns a random real in [0,1).
+    return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+    // Returns a random real in [min,max).
+    return min + (max-min)*random_double();
+}
+
+inline double random_double_alternate() {
+    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
+
+inline double clamp(double x, double min, double max) {
+    if (x < min) return min;
+    if (x > max) return max;
+    return x;
 }
 
 // Common Headers
