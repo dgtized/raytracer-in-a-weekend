05e3074868ea646b6f83ac6cf5a13105ea5e1ef7
Tessa Kelly <tessakkelly@gmail.com>
2020-09-02 18:34:00 -0700

    Adds dielectric and makes two of the balls fully refractive

--- src/material.hpp
+++ src/material.hpp
@@ -48,4 +48,24 @@ public:
   double fuzz;
 };
 
+class dielectric : public material {
+public:
+  dielectric(double ri) : ref_idx(ri) {}
+
+
+  virtual bool scatter(const ray& r_in, const hit_record& rec,
+                       color& attenuation, ray& scattered
+                       ) const override {
+    attenuation = color(1.0,1.0,1.0);
+    double etai_over_eta = rec.front_face ? (1.0 / ref_idx) : ref_idx;
+
+    vec3 unit_direction = unit_vector(r_in.direction());
+    vec3 refracted = refract(unit_direction, rec.normal, etai_over_eta);
+    scattered = ray(rec.p, refracted);
+    return true;
+  }
+
+  double ref_idx;
+};
+
 #endif
