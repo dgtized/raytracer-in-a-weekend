04ae92caba82193042d917fa76c1dd9a0ba3981a
Charles Comstock <dgtized@gmail.com>
2020-11-11 20:17:49 -0600

    Remove -pg profiling from makefile and put it in profile.sh

--- profile.sh
+++ profile.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-# Using gprof2dot.py
-# https://raw.githubusercontent.com/jrfonseca/gprof2dot/master/gprof2dot.py
+make clean
+g++ -std=c++11 -pg src/main.cc -o raytracer
+./render.sh
 
 # Flat Graph
 gprof raytracer gmon.out | head -20 > profiles/201111.random_scene.flat
 
+# Using gprof2dot.py
+# https://raw.githubusercontent.com/jrfonseca/gprof2dot/master/gprof2dot.py
 # Call Graph Visualization
 gprof --graph raytracer gmon.out | python gprof2dot.py |
     tee profiles/201111.random_scene.dot |
