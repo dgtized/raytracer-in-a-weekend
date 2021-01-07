6c3a1a255a2bb2111ae7cbb03bcab35714c7857f
Tessa Kelly <tessakkelly@gmail.com>
2021-01-06 18:04:12 -0800

    Adds moving sphere

--- src/camera.hpp
+++ src/camera.hpp
@@ -12,7 +12,9 @@ public:
     double vfov, // vertical field-of-view in degrees
     double aspect_ratio,
     double aperture,
-    double focus_dist
+    double focus_dist,
+    double _time0 = 0,
+    double _time1 = 0
   ) {
     auto theta = degrees_to_radians(vfov);
     auto h = tan(theta/2);
@@ -28,6 +30,8 @@ public:
     vertical = focus_dist * viewport_height * v;
     lower_left_corner = origin - horizontal / 2 - vertical / 2 - focus_dist*w;
     lens_radius = aperture / 2;
+    time0 = _time0;
+    time1 = _time1;
   }
 
   ray get_ray(double s, double t) const {
@@ -35,7 +39,8 @@ public:
     vec3 offset = u * rd.x() + v * rd.y();
     //std::cerr << u << v << "offset: " << offset << "rd: " << rd << std::endl;
     return ray(origin + offset,
-               lower_left_corner + s * horizontal + t * vertical - origin - offset);
+               lower_left_corner + s * horizontal + t * vertical - origin - offset,
+               random_double(time0, time1));
   }
 
 private:
@@ -45,6 +50,7 @@ private:
   vec3 vertical;
   vec3 u, v, w;
   double lens_radius;
+  double time0, time1; // shutter open/close times
 };
 
 #endif
