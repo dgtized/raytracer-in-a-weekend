03d47a3a6c88c921916c65d5294f414a27160f88
Charles Comstock <dgtized@gmail.com>
2021-02-03 20:42:36 -0600

    Add bounding_box implementations for hittable_list, sphere, and moveable_sphere

--- src/hittable_list.hpp
+++ src/hittable_list.hpp
@@ -2,6 +2,7 @@
 #define HITTABLE_LIST_H
 
 #include "hittable.hpp"
+#include "aabb.hpp"
 
 #include <memory>
 #include <vector>
@@ -18,6 +19,7 @@ public:
   void add(shared_ptr<hittable> object) { objects.push_back(object); }
 
   virtual bool hit(const ray& r, double tmin, double tmax, hit_record& rec) const override;
+  virtual bool bounding_box(double time0, double time1, aabb& output_box) const override;
 
 public:
   std::vector<shared_ptr<hittable>> objects;
@@ -39,6 +41,21 @@ bool hittable_list::hit(const ray& r, double t_min, double t_max, hit_record& re
   }
 
   return hit_anything;
+}
+
+bool hittable_list::bounding_box(double time0, double time1, aabb& output_box) const {
+  if (objects.empty()) return false;
+
+  aabb temp_box; // default to bounding box containing only origin
+  bool first_box = true;
+
+  for (const auto& object : objects) {
+    if (!object->bounding_box(time0, time1, temp_box)) return false;
+    output_box = first_box ? temp_box : surrounding_box(output_box, temp_box);
+    first_box = false;
+  }
+
+  return true;
 }
 
 #endif
--- src/sphere.hpp
+++ src/sphere.hpp
@@ -11,6 +11,7 @@ public:
     center(cen), radius(r), mat_ptr(m) {};
 
   virtual bool hit(const ray& r, double tmin, double tmax, hit_record& rec) const override;
+  virtual bool bounding_box(double time0, double time1, aabb& output_box) const override;
 
 public:
   point3 center;
@@ -53,5 +54,10 @@ bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) cons
   return false;
 }
 
+bool sphere::bounding_box(double time0, double time1, aabb& output_box) const {
+  output_box = aabb(center - vec3(radius, radius, radius),
+                    center + vec3(radius, radius, radius));
+  return true;
+}
 
 #endif
