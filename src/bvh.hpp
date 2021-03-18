c2809b7786d83a7c827bb79153506680328b36b9
Charles Comstock <dgtized@gmail.com>
2021-03-17 20:16:58 -0500

    Implement box_compare functions

--- src/bvh.hpp
+++ src/bvh.hpp
@@ -7,6 +7,27 @@
 #include "hittable_list.hpp"
 #include <algorithm>
 
+inline bool box_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b, int axis) {
+  aabb box_a, box_b;
+
+  if (!a->bounding_box(0,0, box_a) || !b->bounding_box(0,0, box_b)) {
+    std::cerr << "No bounding box in bvh box compare.\n";
+  }
+  return box_a.min().e[axis] < box_b.min().e[axis];
+}
+
+bool box_x_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
+  return box_compare(a, b, 0);
+}
+
+bool box_y_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
+  return box_compare(a, b, 1);
+}
+
+bool box_z_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
+  return box_compare(a, b, 2);
+}
+
 class bvh_node : public hittable {
 public:
   bvh_node();
