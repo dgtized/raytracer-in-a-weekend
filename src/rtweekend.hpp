ffbdc672dfbe1f8f27ca3d0fafbca36c7875742b
Tessa Kelly <tessakkelly@gmail.com>
2021-03-17 18:06:31 -0700

    Adds random_int

--- src/rtweekend.hpp
+++ src/rtweekend.hpp
@@ -41,6 +41,11 @@ inline double random_double_alternate() {
     return distribution(generator);
 }
 
+inline int random_int(int min, int max) {
+    // Returns a random integer in [min,max].
+    return static_cast<int>(random_double(min, max+1));
+}
+
 inline double clamp(double x, double min, double max) {
     if (x < min) return min;
     if (x > max) return max;
