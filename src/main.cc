f5b49930c2f0f1870f5a08f68ae7abc03aa58787
Tessa Kelly <tessakkelly@gmail.com>
2021-03-31 18:03:25 -0700

    Adds checkers to this checkered world

--- src/main.cc
+++ src/main.cc
@@ -63,7 +63,8 @@ hittable_list camera_fov_test() {
 hittable_list random_scene() {
   hittable_list world;
 
-  auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+  auto checker = make_shared<checker_texture>(color(0.2, 0.3, 0.1), color(0.9,0.9,0.9));
+  auto ground_material = make_shared<lambertian>(checker);
   world.add(make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
 
   for (int a = -11; a < 11; a++) {
