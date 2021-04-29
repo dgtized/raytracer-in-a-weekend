ca67a8eacb53efcc63e3655bf32a853380dd8df6
Charles Comstock <dgtized@gmail.com>
2021-04-28 20:02:28 -0500

    Enable marbling from perlin noise

--- src/texture.hpp
+++ src/texture.hpp
@@ -58,7 +58,7 @@ public:
   noise_texture(double sc) : scale(sc) {}
 
   virtual color value(double u, double v, const point3& p) const override {
-    return color(1,1,1) * noise.turbulence(scale * p);
+    return color(1,1,1) * 0.5 * (1.0 + sin(scale*p.z() + 10 * noise.turbulence(p)));
   }
 
 public:
